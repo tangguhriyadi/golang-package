42e59e160e8a2e55300a8c2da58f8e5bac7a314e
tangguhriyadi <mtangguh97@gmail.com>
2023-02-16 11:16:26 +0700

    package

---
+++ os.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println(os.Args)
+
+	hostname, err := os.Hostname()
+
+	if err == nil {
+		fmt.Println("hostname", hostname)
+	} else {
+		fmt.Println("err", err)
+	}
+}
