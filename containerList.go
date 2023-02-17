f0e29b5191ae2e335f7ed6056c74a3007846c680
tangguhriyadi <mtangguh97@gmail.com>
2023-02-17 09:12:30 +0700

    container list

---
+++ containerList.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+	data := list.New()
+	data.PushBack("muhammad")
+	data.PushBack("tangguh")
+	data.PushBack("riyadi")
+
+	fmt.Println(data.Front().Value)
+	fmt.Println(data.Front().Next().Value)
+	fmt.Println(data.Back().Value)
+
+	/* for element := data.Front(); element != nil; element = element.Next() {
+		fmt.Println(element.Value)
+	} */
+	for element := data.Back(); element != nil; element = element.Prev() {
+		fmt.Println(element.Value)
+	}
+
+}
